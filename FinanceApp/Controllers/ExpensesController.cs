4a38fa9c4923438c0160d6e13a77d7a4ad3321e1
Kenneth <kenneth.sulaiman@gmail.com>
2025-11-06 02:30:18 +0700

    initial commit

---
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,46 @@
+﻿using FinanceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+using FinanceApp.Data.Service;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly IExpensesService _expensesService;
+        public ExpensesController(IExpensesService expensesService)
+        {
+            _expensesService = expensesService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var expenses = await _expensesService.GetAll();
+            return View(expenses);
+        }
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+        [HttpPost]
+		public async Task<IActionResult> Create(Expense expense)
+        {
+            if(ModelState.IsValid)
+            {
+                await _expensesService.Add(expense);
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult GetChart()
+        {
+            var data = _expensesService.GetChartData();
+
+            return Json(data);
+        }
+    }
+}
