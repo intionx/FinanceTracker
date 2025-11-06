49428253bf4c9148338ef1addf4958d9840e3220
Kenneth <kenneth.sulaiman@gmail.com>
2025-11-07 03:04:09 +0700

    updated home page to expense page

--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Expenses}/{action=Index}/{id?}");
 
 app.Run();
